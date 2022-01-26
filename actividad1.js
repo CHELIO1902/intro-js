a39e47f55783b7b5700d358ee2332138e99814b6
CHELIO1902 <chelio19022012@hotmail.com>
2022-01-25 21:45:31 -0600

    4 ejercicios resueltos

---
+++ actividad1.js
@@ -0,0 +1,24 @@
+// ejercicio 1
+var input = prompt("ingresa un número")
+var multiplicacion = 2
+var resultado = input * multiplicacion
+console.log(total)
+
+// ejercicio 2 
+var input1 = prompt("escribe 1er número")
+var input2 = prompt("ingresa 2do número")
+var resultado = input1 - input2
+console.log(resultado)
+
+//ejercicio 3
+var radio = (5.3)**2
+var pi = 3.1416
+var resultado = radio * pi
+console.log(resultado)
+
+//ejercicio 4
+var diagonalMayor = 8
+var diagonalMenor = 6
+var division = 2
+var resultado = diagonalMayor * diagonalMenor / division
+console.log(resultado)
