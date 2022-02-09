753ed1b8fdc4b78636ba523e478cfd313c0bcee9
CHELIO1902 <chelio19022012@hotmail.com>
2022-02-09 14:58:45 -0600

    actualizacion de actividad 9

--- actividad9/actividad9.1.js
+++ actividad9/actividad9.1.js
@@ -3,106 +3,106 @@
 
 //1
 //  * Del siguiente objeto:
- /*    var student = {
+    var student = {
         name: "David Rayy",
         age: 34,
         job: 'engineer',
         hobbies: ['Sky', 'football', 'walking my dog']
-    }; */
+    };
 
  /*    1. Muestra en pantalla el valor de la propiedad name mediante dot notation
     2. Muestra en pantalla el valor de la propiedad age mediante bracket notation
     3. Modifica el valor de la propiedad name y que el nuevo valor sea: "Steve Jobs"
  */
-/* console.log(student.name)
-console.log(student[1])
+console.log(student.name)
+console.log(student["age"])
 student.name = "Steve Jobs"
-console.log(student.name) */
+console.log(student.name)
 
 
 // 2
  /* Escriba un programa de JavaScript para enumerar las propiedades de un objeto de JavaScript.
     Objeto de muestra:*/
 
-/*     var student = {
+    var student = {
         name: "David Rayy",
         sclass: "VI",
         rollno: 12
     };
     
-    console.log(Object.keys(student)); */
+    console.log(Object.keys(student));
 
 
 //3
 //Verifica si la propiedad name se encuentra dentro del siguiente objeto:
- /*     var student = {
+     var student = {
         name: "David Rayy",
         sclass: "VI",
         rollno: 12
     };
 
-    console.log(student.hasOwnProperty('name')) //copie la soluciión de usted
- */
+    console.log(student.hasOwnProperty('name')) //copie la soluciión de usted, es el unico ejercicio que me faltaba
+
 
 
 //4
 /*Escriba un programa de JavaScript para eliminar la propiedad rollno del siguiente objeto. 
  * Imprima también el objeto antes o después de eliminar la propiedad. 
- * Objeto para usar:*/
-  /* var student = {
+/* /* /*  * Objeto para usar:*/
+  var student = {
         name: "David Rayy",
         sclass: "VI",
         rollno: 12
     };
 
     delete student.rollno
-    console.log(student) */
+    console.log(student) 
 
 
 //5
  //Escriba un programa para modificar el valor de la segunda propiedad del siguiente objeto:
  
-/*      var student = {
+     var student = {
         name: "David Rayy",
         age: 34,
         job: 'engineer',
         hobbies: ['Sky', 'football', 'walking my dog']
-    }; */
+    };
 //La nueva edad debe ser 35.
 
-/* student.age = 35
-console.log(student) */
+student.age = 35
+console.log(student)
 
 
 
 
 //6
  //Escriba un programa para añadir una nueva propiedad al siguiente objeto:
-  /*   var student = {
+    var student = {
         name: "David Rayy",
         age: 34,
         job: 'engineer',
         hobbies: ['Sky', 'football', 'walking my dog']
-    }; */
+    };
 
 //La nueva propiedad debe ser: pet y agregue un valor, ejemplo: cat, dog.
  
-/* student.pet = "dog"
-console.log(student) */
+student.pet = "dog"
+console.log(student)
 
 
 //7
 // Crea un código para llenar un objecto vacio:
  
-// const objeto1 = {}
+ const objeto1 = {}
   
- /* Con las siguiente propiedades:
+  Con las siguiente propiedades:
     * color
     * tamaño
     * peso
     * cantidad
     */
-/* Object.defineProperties(objeto1, {
+ Object.defineProperties(objeto1, {
   color: {
     enumerable: true,
     value: "amarillo",
@@ -125,7 +125,7 @@ console.log(student) */
   }
 })
 
-console.log(objeto1) */
+console.log(objeto1)
 
 
 
---
+++ actividad9/ejemplo9.2.js
@@ -0,0 +1,7 @@
+const arr = [2, true, "hola"]
+
+//forEach
+
+arr.forEach(function (element){
+  console.log(element)
+})
