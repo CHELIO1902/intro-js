43f9d2ed711644d7c7790b50c081809ac8960d88
CHELIO1902 <chelio19022012@hotmail.com>
2022-02-17 18:31:41 -0600

    cajero automatico al 95%

--- proyecto-cajero-automatico/ca-js.js
+++ proyecto-cajero-automatico/ca-js.js
@@ -1,23 +1,75 @@
+//funcion para guardar el usuario en el sesion storage
+
+function usuario (){
+ let nombre = sessionStorage.getItem
+} 
+
 
 // clientes disponibles
+//esta variable nos va a extraer el dato que el navegador guardo en su memoria para poder usarlo con las demas funciones y asi validad que usuario fue el que se logueo.
+var usuarioLogueado = localStorage.getItem('usuario');
 
 var cuentas = [
-  { nombre: "Mali", saldo: 200 }
-  { nombre: "Gera", saldo: 290 }
+  { nombre: "Mali", saldo: 200 },
+  { nombre: "Gera", saldo: 290 },
   { nombre: "Maui", saldo: 67 }
 ];
 
 function displayUsuario (cliente){
- for(var = i; i < cliente.length; i++)
-  if (document.getElementById("usuario").innerHTML == cliente[i].name){
-    document.getElementById("output").innerHTML += "Hola " + cliente[i].name + "tu saldo es de $" + cliente[i].saldo + "pesos"
+for(var i = 0; i < cuentas.length; i++)
+  if (cliente == cuentas[i].nombre){
+    document.getElementById("output").innerHTML = "Hola " + cuentas[i].nombre + " tu saldo es de $" + cuentas[i].saldo + " pesos"
   }
 }
 
-displayUsuario(cuentas)
+displayUsuario(usuarioLogueado)
+
+
+//obtener los datos del imput
+//buscar el cliente logueado dentro de las cuentas
+//sumar el valor del input a las cuentas globales
+// mostrar en pantalla
 
 
 
-function muestraSaldo (){
-  document.getElementById("consultarSaldo").innerHTML = displayUsuario(cuentas)
+function depositarSaldo (cantidad, cliente){
+  for(var i = 0; i < cuentas.length; i++){
+    if(cliente == cuentas[i].nombre){
+      cuentas[i].saldo += Number(cantidad)
+    }
+  }
 } 
+//funcion para obtener los datos del input
+
+function agregarSaldo(){
+  let input = document.getElementById("ingresoCantidad").value
+  depositarSaldo(input, usuarioLogueado)
+  displayUsuario(usuarioLogueado)
+  document.getElementById("ingresoCantidad").value = " "
+}
+
+//funcion para retirar saldo
+
+function retirarSaldo(){
+  let input = document.getElementById("retiroCantidad").value
+  retiro(input, usuarioLogueado)
+  displayUsuario(usuarioLogueado)
+  document.getElementById("retiroCantidad").value = " "
+}
+
+function retiro (cantidad, cliente){
+  for(var i = 0; i < cuentas.length; i++){
+    if(cliente == cuentas[i].nombre){
+      cuentas[i].saldo -= Number(cantidad)
+    }
+  }
+} 
+
+// boton cierre de cesion
+
+function cierreSesion (){
+  let cierre1 = location.href ="login.html"
+  // sirve para borrar los datos almancenados en la base de datos del navegador
+   localStorage.clear()
+}
+
--- proyecto-cajero-automatico/login.js
+++ proyecto-cajero-automatico/login.js
@@ -3,13 +3,17 @@
 function inicioSesion(){
   event.preventDefault()
   let usuario = document.getElementById("usuario").value
-  let contraseña = document.getElementById("contraseña").value
+  let contrasena = document.getElementById("contraseña").value
+  //se declara la variable miStorage 
+  let miStorage = window.localStorage
+  // se le agregan valores ("usuario" es una key, y la variable usuario ya esta delcarada que es la que nos va a dar el valor que necesitamos guardar 
+      miStorage.setItem("usuario", usuario)
 
-  if (usuario == "Mali" && contraseña == "123456"){
+if (usuario == "Mali" && contrasena == "123456"){
     location.href ="ca-index.html"
-  }else if (usuario == "Gera" && contraseña == "234567"){
+  }else if (usuario == "Gera" && contrasena == "234567"){
     location.href ="ca-index.html"
-  }else if (usuario == "Maui" && contraseña == "345678"){
+  }else if (usuario == "Maui" && contrasena == "345678"){
     location.href ="ca-index.html"
   }else{
     alert("Revisa tu usuario o contraseña")
